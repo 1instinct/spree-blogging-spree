6c1f68eb4aa0181bd68dae6411046d51c635c7dc
Aaron Smulktis <aaronsmulktis@gmail.com>
2019-07-22 20:51:28 -0700

    add :originals to overrides (prob use-case specific), and add a missing translation? idk seemed like it was already there but could work differently than Id imagine

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,7 +1,8 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                     :original => 'e48585c7f540e0c388f4c9b7480a84b5f56003e4',
                      :name => "blog_admin_tabs",
                      :insert_bottom => "#main-sidebar",
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
+                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:blog_menu_title), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
                      :disabled => false)
 
 
--- app/overrides/add_user_admin_author_info_fields.rb
+++ app/overrides/add_user_admin_author_info_fields.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/users/_form",
+                     :original => '150f89cabfb7f704dd5283be6e650f51ca025e2f',
                      :name => "admin_author_info_fields",
                      :insert_bottom => "[data-hook='admin_user_form_fields']",
                      :partial => "spree/admin/users/author_info_fields",
